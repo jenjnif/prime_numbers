9e24916498a215144a0b09ca1d94aec985c2bfae
jenjnif <jenjnif@hotmail.com>
2019-01-25 21:58:05 +0000

    write test for a list of prime numbers to n

---
+++ finding_unique_primes.py
@@ -0,0 +1,15 @@
+# finds all unique primes factors of any number, n
+# e.g. if n = 500, if only has 4 unique prime factors (that do not
+# belong to any number less than 500).
+
+
+def find_primes(n):
+    pass
+
+
+def is_prime(x):
+    pass
+
+
+def test_list_prime_to_n():
+    assert is_prime(20) == [2, 3, 5, 7, 11, 13, 17, 19]
