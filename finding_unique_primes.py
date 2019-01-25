8a97a26a1e4188019266c08c2309aa5dca6ebaf1
jenjnif <jenjnif@hotmail.com>
2019-01-25 22:16:04 +0000

    pass test to produce list of primes to n

--- finding_unique_primes.py
+++ finding_unique_primes.py
@@ -4,11 +4,21 @@
 
 
 def find_primes(n):
-    pass
+    primes = []
+    for i in range(2, n):
+        if is_prime(i):
+            primes.append(i)
+    print(primes)
+    return(primes)
 
 
 def is_prime(x):
-    pass
+    prime = True
+    for i in range(2, x):
+        if x % i == 0:
+            prime = False
+    print(prime)
+    return prime
 
 
 def test_list_prime_to_n():
