56ef490e183768aa36913e38cb73fca8dda6b4eb
jenjnif <jenjnif@hotmail.com>
2019-01-26 10:21:19 +0000

    Update unique prime factors test

--- finding_unique_primes.py
+++ finding_unique_primes.py
@@ -8,7 +8,6 @@ def find_primes(n):
     for i in range(2, n):
         if is_prime(i):
             primes.append(i)
-    print(primes)
     return(primes)
 
 
@@ -17,9 +16,16 @@ def is_prime(x):
     for i in range(2, x):
         if x % i == 0:
             prime = False
-    print(prime)
     return prime
+
+
+def unique_prime_factors():
+    pass
 
 
 def test_list_prime_to_n():
     assert find_primes(20) == [2, 3, 5, 7, 11, 13, 17, 19]
+
+
+def test_unique_prime_factor():
+    assert unique_prime_factors(20) == [2, 3]
