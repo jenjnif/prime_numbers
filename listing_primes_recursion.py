3c2fc7cfbfc99254d6b9ae2844ae8a65b9d14de7
jenjnif <jenjnif@hotmail.com>
2019-01-30 09:07:33 +0000

    Correct values for list of 1 to n using for loop

--- listing_primes_recursion.py
+++ listing_primes_recursion.py
@@ -1,7 +1,7 @@
 # list all the numbers from 1 to n using for loop
 def list_numbers(n):
     numbers = []
-    for i in range(n):
+    for i in range(1, n + 1):
         numbers.append(i)
     return numbers
 
@@ -11,7 +11,6 @@ print(list_numbers(10))
 
 # list all the numbers from 1 to n using recursion
 def rec_list_numbers(n):
-    print(n)
     if n == 0:
         return []
     return rec_list_numbers(n-1) + [n]
@@ -21,21 +20,22 @@ print(rec_list_numbers(10))
 
 
 # list all the PRIME numbers from 1 to n using a for loop
-def listing_primes(n):
-    primes = []
-    for i in range(2, n + 1):
-        prime = True
-        for multiple in range(2, i):
-            if i % multiple == 0:
-                prime = False
-        if prime is True:
-            primes.append(i)
-    return primes
+# def listing_primes(n):
+#     primes = []
+#     for i in range(2, n + 1):
+#         prime = True
+#         for multiple in range(2, i):
+#             if i % multiple == 0:
+#                 prime = False
+#         if prime is True:
+#             primes.append(i)
+#     return primes
 
 
-print(listing_primes(10))
+# print(listing_primes(10))
 
-# list all the PRIME numbers from 1 to n using a for loop
+
+# # list all the PRIME numbers from 1 to n using recursion
 # def listing_primes_recursion(n):
 #     primes = []
 #     for i in range(2, n + 1):
@@ -50,4 +50,4 @@ print(listing_primes(10))
 #     return primes
 
 
-# listing_primes_recursion(10)
+# print(listing_primes_recursion(10))
